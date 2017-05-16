a15b60598e7cc6df7c6c952b5e1af62fd5fc3c3c
Timo Vilppu <timo.vilppu@arado.fi>
2017-05-16 06:23:55 +0300

    encapsulate HttpClient as Http module

---
+++ Utility/Http.fs
@@ -0,0 +1,16 @@
+namespace YogRobot
+
+module Http = 
+    open System
+    open System.Net
+    open System.Net.Http
+    open System.Net.Http.Headers
+    open System.Text    
+    open System.Threading.Tasks
+    
+    let private httpClient = new HttpClient()
+
+    let Send (request : HttpRequestMessage) : Task<HttpResponseMessage> =
+        httpClient.SendAsync request
+
+    
