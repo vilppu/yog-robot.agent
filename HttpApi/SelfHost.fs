d297ca2c3b179708f8fadca120dc9aa8622c12f1
vilppu <tsvilp@gmail.com>
2017-12-06 18:57:38 +0200

    upgraded to dotnet core 2

--- HttpApi/SelfHost.fs
+++ HttpApi/SelfHost.fs
@@ -4,54 +4,48 @@
 module SelfHost = 
     open System
     open System.IO
-    open System.Net
     open System.Net.Http
-    open System.Threading
     open System.Threading.Tasks    
     open Microsoft.AspNetCore.Authorization
-    open Microsoft.AspNetCore.Authentication.JwtBearer
+    open Microsoft.AspNetCore.Authentication;
+    open Microsoft.AspNetCore.Authentication.JwtBearer;
     open Microsoft.AspNetCore.Builder
     open Microsoft.AspNetCore.Hosting
     open Microsoft.AspNetCore.Mvc
-    open Microsoft.Extensions.Configuration
     open Microsoft.Extensions.DependencyInjection
     open Microsoft.Extensions.Logging
-    open Microsoft.Extensions.Configuration
     open Microsoft.IdentityModel.Tokens
     open Newtonsoft.Json
     open Newtonsoft.Json.Serialization
+        
+    let private GetUrl() =
+        
+        let configuredUrl = Environment.GetEnvironmentVariable("YOG_BOT_BASE_URL")
+        let url = 
+            if String.IsNullOrWhiteSpace(configuredUrl) then "http://localhost:18888/yog-robot"
+            else configuredUrl
+
+        new Uri(url)
     
     type Startup(environment : IHostingEnvironment) =
-        
+
         member this.Configure(app : IApplicationBuilder, env : IHostingEnvironment, loggerFactory : ILoggerFactory, httpSend : HttpRequestMessage -> Task<HttpResponseMessage>) =             
             loggerFactory
                 .AddConsole(LogLevel.Warning)
                 .AddDebug()
-                |> ignore
-                
-            let jwtBearerOptions =
-                let tokenValidationParameters = TokenValidationParameters()
-                tokenValidationParameters.ValidateIssuerSigningKey <- true
-                tokenValidationParameters.IssuerSigningKey <- SigningKey
-                tokenValidationParameters.ClockSkew <- TimeSpan.Zero
-                tokenValidationParameters.ValidateIssuer <- false
-                tokenValidationParameters.ValidIssuer <- "NotUsed"
-                tokenValidationParameters.ValidateAudience <- false
-                tokenValidationParameters.ValidAudience <- "NotUsed"
-                tokenValidationParameters.ValidateLifetime <- false
-                let options = JwtBearerOptions()
-                options.AutomaticAuthenticate <- true
-                options.AutomaticChallenge <- true
-                options.TokenValidationParameters <- tokenValidationParameters
-                options
+                |> ignore            
 
-            app.UseJwtBearerAuthentication(jwtBearerOptions).UseMvc()
-            |> ignore
+            app
+                .UsePathBase(new Microsoft.AspNetCore.Http.PathString(GetUrl().PathAndQuery))
+                .UseAuthentication()
+                .UseMvc()
+                |> ignore
             
         member this.ConfigureServices(services : IServiceCollection) =
             let configureJson (options : MvcJsonOptions) = 
                 options.SerializerSettings.ContractResolver <- CamelCasePropertyNamesContractResolver()
-            let configureJsonAction = new Action<MvcJsonOptions>(configureJson)
+            let configureJsonAction = new Action<MvcJsonOptions>(configureJson)            
+
             services
                 .AddMvc()
                 .AddJsonOptions(configureJsonAction)
@@ -74,23 +68,39 @@ module SelfHost =
                     fun (policy : AuthorizationPolicyBuilder) ->
                         policy.Requirements.Add(PermissionRequirement(Roles.Sensor))
                 new Action<AuthorizationPolicyBuilder>(builder)
-
             
             services.AddAuthorization(fun options ->
                 options.AddPolicy(Roles.Administrator, configureAdminPolicy)
                 options.AddPolicy(Roles.User, configureUserPolicy)
                 options.AddPolicy(Roles.Sensor, configureSensorPolicy)
-            ) |> ignore
+            ) |> ignore               
+                
+            let tokenValidationParameters = TokenValidationParameters()
+            tokenValidationParameters.ValidateIssuerSigningKey <- true
+            tokenValidationParameters.IssuerSigningKey <- SigningKey
+            tokenValidationParameters.ClockSkew <- TimeSpan.Zero
+            tokenValidationParameters.ValidateIssuer <- false
+            tokenValidationParameters.ValidIssuer <- "NotUsed"
+            tokenValidationParameters.ValidateAudience <- false
+            tokenValidationParameters.ValidAudience <- "NotUsed"
+            tokenValidationParameters.ValidateLifetime <- false
+
+            services
+                .AddAuthentication(fun options -> 
+                    options.DefaultAuthenticateScheme <- JwtBearerDefaults.AuthenticationScheme
+                    options.DefaultChallengeScheme <- JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(fun options ->
+                    options.TokenValidationParameters <- tokenValidationParameters
+                    )
+                |> ignore
 
             services.AddSingleton<IAuthorizationHandler, PermissionHandler>()
             |> ignore
 
     let CreateHttpServer (httpSend : HttpRequestMessage -> Task<HttpResponseMessage>) : Task = 
-        let url = Environment.GetEnvironmentVariable("YOG_BOT_BASE_URL")
-        
-        let url = 
-            if String.IsNullOrWhiteSpace(url) then "http://localhost:18888/yog-robot/"
-            else url
+
+        let url = GetUrl()
+        let host = url.Scheme + Uri.SchemeDelimiter + url.Host + ":" + url.Port.ToString()
 
         let host = 
             WebHostBuilder()
@@ -98,8 +108,7 @@ module SelfHost =
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
-                .UseUrls(url)
+                .UseUrls(host)
                 .Build()
 
         Task.Run(fun () -> host.Run())
-        
