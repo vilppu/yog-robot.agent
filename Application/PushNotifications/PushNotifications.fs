57152135690c436bf988ef504273b9871aba1f80
vilppu <tsvilp@gmail.com>
2018-01-13 13:38:26 +0200

    send push notifications about presene of water

--- Application/PushNotifications/PushNotifications.fs
+++ Application/PushNotifications/PushNotifications.fs
@@ -9,28 +9,36 @@ module PushNotification =
           // Sensor status after the event
           Status : StorableSensorStatus }
 
-    let private sendPushNotifications httpSend reason =
-        let sensorName =
-            if reason.Status :> obj |> isNull then reason.Event.SensorId.AsString
-            else reason.Status.SensorName
-        let measurement = StorableMeasurement reason.Event.Measurement        
-        let sendFirebaseMessages = SendFirebaseMessages httpSend reason.Event.DeviceGroupId
-        let pushNotification : DevicePushNotification =
-            { DeviceId = reason.Event.DeviceId.AsString
-              SensorName = sensorName
-              MeasuredProperty = measurement.Name
-              MeasuredValue = measurement.Value
-              Timestamp = reason.Event.Timestamp }
-        pushNotification |> sendFirebaseMessages
+    let private sendFirebasePushNotifications httpSend reason =
+        async {
+            let measurement = StorableMeasurement reason.Event.Measurement
+            let sensorName =
+                if reason.Status :> obj |> isNull then reason.Event.SensorId.AsString
+                else reason.Status.SensorName
+            let sendFirebaseMessages = SendFirebaseMessages httpSend reason.Event.DeviceGroupId
+            let pushNotification : DevicePushNotification =
+                { DeviceId = reason.Event.DeviceId.AsString
+                  SensorName = sensorName
+                  MeasuredProperty = measurement.Name
+                  MeasuredValue = measurement.Value
+                  Timestamp = reason.Event.Timestamp }
+            do! sendFirebaseMessages pushNotification
+        }
 
-    let SendPushNotifications httpSend reason =
+    let private sendPushNotificationsAboutChange httpSend reason =
         async {
             let measurement = StorableMeasurement reason.Event.Measurement
             let hasChanged =
                 if reason.Status :> obj |> isNull then true
-                else measurement.Value <> reason.Status.MeasuredValue 
+                else measurement.Value <> reason.Status.MeasuredValue
             if hasChanged then
-                match reason.Event.Measurement with
-                | Contact contact -> do! sendPushNotifications httpSend reason
-                | _ -> ()
+                do! sendFirebasePushNotifications httpSend reason
+        }
+
+    let SendPushNotifications httpSend reason =
+        async {
+            match reason.Event.Measurement with
+            | Contact _ -> do! sendPushNotificationsAboutChange httpSend reason
+            | PresenceOfWater _ -> do! sendPushNotificationsAboutChange httpSend reason
+            | _ -> ()
         }
--- Tests/PushNotificationTests.fs
+++ Tests/PushNotificationTests.fs
@@ -36,17 +36,66 @@ module PushNotificationTests =
           mutable results : List<FirebaseResult> }
 
     [<Fact>]
-    let NotifyWhenContactChanges() = 
+    let NotifyAboutContact() = 
         use context = SetupContext()
-        let example = Contact Contact.Open
+        let opened = Contact Contact.Open
+        let closed = Contact Contact.Closed
 
         context |> SetupToReceivePushNotifications
 
-        context |> WriteMeasurementSynchronously(Fake.Measurement example)
+        context |> WriteMeasurementSynchronously(Fake.Measurement opened)
+        context |> WriteMeasurementSynchronously(Fake.Measurement closed)
         
         // Wait for background processing to complete.
         System.Threading.Thread.Sleep(100)
 
-        Assert.Equal(1, SentHttpRequests.Count)
+        Assert.Equal(2, SentHttpRequests.Count)
         Assert.Equal("https://fcm.googleapis.com/fcm/send", SentHttpRequests.[0].RequestUri.ToString())
+
+    let WaitForBackgroundProcessingToComplete =
+        System.Threading.Thread.Sleep(100)
+
+    [<Fact>]
+    let NotifyOnlyWhenContactChanges() = 
+        use context = SetupContext()
+        let opened = Contact Contact.Open
+
+        context |> SetupToReceivePushNotifications
+
+        context |> WriteMeasurementSynchronously(Fake.Measurement opened)        
+        context |> WriteMeasurementSynchronously(Fake.Measurement opened)
+
+        WaitForBackgroundProcessingToComplete
+
+        Assert.Equal(1, SentHttpRequests.Count)
+
+    [<Fact>]
+    let NotifyAboutPresenceOfWater() = 
+        use context = SetupContext()
+        let present = PresenceOfWater PresenceOfWater.Present
+        let notPresent = PresenceOfWater PresenceOfWater.NotPresent
+
+        context |> SetupToReceivePushNotifications
+        
+        context |> WriteMeasurementSynchronously(Fake.Measurement present)
+        context |> WriteMeasurementSynchronously(Fake.Measurement notPresent)
+        
+        WaitForBackgroundProcessingToComplete
+
+        Assert.Equal(2, SentHttpRequests.Count)
+        Assert.Equal("https://fcm.googleapis.com/fcm/send", SentHttpRequests.[0].RequestUri.ToString())
+
+    [<Fact>]
+    let NotifyOnlyWhenPresenceOfWaterChanges() = 
+        use context = SetupContext()
+        let present = PresenceOfWater PresenceOfWater.Present
+
+        context |> SetupToReceivePushNotifications
+
+        context |> WriteMeasurementSynchronously(Fake.Measurement present)
+        context |> WriteMeasurementSynchronously(Fake.Measurement present)
+        
+        WaitForBackgroundProcessingToComplete
+
+        Assert.Equal(1, SentHttpRequests.Count)
    
