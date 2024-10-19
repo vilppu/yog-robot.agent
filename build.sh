a62205da42fada473bad1e0daaea2d1ef3a439bd
vilppu <tsvilp@gmail.com>
2024-10-19 23:16:04 +0300

    Build as system agnostic

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-dotnet restore ./Tests
-dotnet test ./Tests/Tests.fsproj -c Release
-dotnet publish ./HttpApi -c Release -r debian.8-x64 -o Published
-zip -r yog-robot.agent.zip ./HttpApi/Published
