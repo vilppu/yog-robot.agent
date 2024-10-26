8065393040a885ad3679d89bf305d956ff5fb79b
vilppu <tsvilp@gmail.com>
2024-10-26 17:28:49 +0300

    Add debug logging

--- Application/Action.fs
+++ Application/Action.fs
@@ -58,6 +58,8 @@ module internal Action =
 
     let SendNotifications sendFirebaseMulticastMessages (sensorState: SensorState) : Task<unit> =
         task {
+            printfn "%b" (sensorState |> shouldBeNotified)
+
             if sensorState |> shouldBeNotified then
                 do! Notification.Send sendFirebaseMulticastMessages sensorState
         }
